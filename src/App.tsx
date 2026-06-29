3aedd549f4b365647dd92b17a7aa8056c81f4af0
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-29 07:55:46 +0600

    feat: add visual data visualization charts to analytics dashboard using Recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "sonner";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Toaster richColors position="top-right" closeButton />
     </BrowserRouter>
   );
 }
